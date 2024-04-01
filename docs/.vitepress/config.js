ab0b7aa6dfe7d014563236be1ba8c1212533b3e6
Priya140895 <priya@myattest.com>
2024-04-01 14:24:12 +0530

    Page creation

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,6 +7,24 @@ module.exports = {
             {text: 'Contact', link: '/contact'},
  
             ],
-            sidebar:[{text:'Attest Installation', link: '/about/Attest-installation' }],
+            sidebar:[
+                {
+                text: 'Attest Installation',
+                collapsed: false,
+                    items: [
+                      { text: 'Attest Installation', link: '/about/Attest-installation' } 
+                    ]
+                },
+
+                {
+                    text: 'Attest Functionalities',
+                    collapsed: false,
+                        items: [
+                          { text: 'Attest Track', link: '/about/Attest-Track' } ,
+                          { text: 'Attest Functions', link: '/about/Attest-Functions' } ,
+                        ]
+                },
+                
+                ],
     }
 }
---
+++ docs/about/Attest-Track.md
@@ -0,0 +1,4 @@
+# Attest Track
+
+### Create Tracks to categorize the Test Functions
+### Navigate to Configuration -> Tracks -> Create New Track
