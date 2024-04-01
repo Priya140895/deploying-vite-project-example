f1c8b76f098468fc8c9f1cd9fe341439137ca0ca
Priya140895 <priya@myattest.com>
2024-04-01 16:31:55 +0530

    commit1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base:'/deploying-vite-project/',
+    base:'/deploying-vite-project-example/',
     plugins:[vue()]
 })
